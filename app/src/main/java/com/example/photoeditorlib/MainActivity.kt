078a63d8c635b8941d46fd9ff8e8499e4da56e39
NeoRevolt <dzalfikriali@gmail.com>
2022-12-15 16:03:56 +0700

    Add Project

---
+++ app/src/main/java/com/example/photoeditorlib/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.example.photoeditorlib
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.my_library.ShowDataLibActivity
+import com.example.photoeditorlib.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.apply {
+            btnToLib.setOnClickListener {
+//                Intent(this@MainActivity, Class.forName("com.example.my_library.ShowDataLibActivity")).also {
+//                    it.putExtra(ShowDataLibActivity.EXTRA_DETAIL,"INI DATA")
+//                    startActivity(it)
+//                }
+                Intent(this@MainActivity, ShowDataLibActivity::class.java).also {
+                    it.putExtra(ShowDataLibActivity.EXTRA_DETAIL,"INI DATA")
+                    startActivity(it)
+                }
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/example/photoeditorlib/ShowDataActivity.kt
@@ -0,0 +1,11 @@
+package com.example.photoeditorlib
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ShowDataActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_show_data)
+    }
+}
